84a35a065db378dd0d0341f15d2a24f2d0f9cc12
Silviu Glavan <silviuglv@gmail.com>
2018-02-13 00:17:28 +0000

    project cleanup

--- gulptasks/tasks/build/serve.js
+++
@@ -1,8 +0,0 @@
-var browserSync = require('browser-sync')
-var config = require('../../config').serve.production
-var gulp = require('gulp')
-
-// Serve (gulp serve:production)
-gulp.task('serve:production', ['build:production'], function () {
-  browserSync(config)
-})
